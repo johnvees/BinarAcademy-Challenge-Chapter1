8da076c2c60b158e7966d414a4b2f54cd92dfe39
johnvees <yohanesvelly23@gmail.com>
2022-02-24 13:51:46 +0700

    update on number 7 and done on number 8

--- Soal-7/script.js
+++ Soal-7/script.js
@@ -1,37 +1,38 @@
-console.log("Chapter 1 Challenge Number 7");
+console.log('Chapter 1 Challenge Number 7');
 
 const getTotalPenjualan = (dataPenjualan) => {
+  dataPenjualan = dataPenjualanPakAldi;
   let sum = 0;
-  for (let i = 0; i < dataPenjualanPakAldi.length; i++) {
-    sum += dataPenjualanPakAldi[i].totalTerjual;
-    // console.log(sum);
+  for (let i = 0; i < dataPenjualan.length; i++) {
+    sum += dataPenjualan[i].totalTerjual;
+    console.log(sum);
   }
   return sum;
 };
 
 const dataPenjualanPakAldi = [
   {
-    namaProduct: "Sepatu Futsal Nike Vapor Academy 8",
+    namaProduct: 'Sepatu Futsal Nike Vapor Academy 8',
     hargaSatuan: 760000,
-    kategori: "Sepatu Sport",
+    kategori: 'Sepatu Sport',
     totalTerjual: 90,
   },
   {
-    namaProduct: "Sepatu Warrior Tristan Black Brown High",
+    namaProduct: 'Sepatu Warrior Tristan Black Brown High',
     hargaSatuan: 960000,
-    kategori: "Sepatu Sneaker",
+    kategori: 'Sepatu Sneaker',
     totalTerjual: 37,
   },
   {
-    namaProduct: "Sepatu Warrior Tristan Maroon High ",
-    kategori: "Sepatu Sneaker",
+    namaProduct: 'Sepatu Warrior Tristan Maroon High ',
+    kategori: 'Sepatu Sneaker',
     hargaSatuan: 360000,
     totalTerjual: 90,
   },
   {
-    namaProduct: "Sepatu Warrior Rainbow Tosca Corduroy",
+    namaProduct: 'Sepatu Warrior Rainbow Tosca Corduroy',
     hargaSatuan: 120000,
-    kategori: "Sepatu Sneaker",
+    kategori: 'Sepatu Sneaker',
     totalTerjual: 90,
   },
 ];
---
+++ Soal-8/script.js
@@ -0,0 +1,106 @@
+console.log('Chapter 1 Challenge Number 8');
+
+let idrCurrency = Intl.NumberFormat('id-ID', {
+  style: 'currency',
+  currency: 'IDR',
+});
+
+const getInfoPenjualan = (dataPenjualan) => {
+  dataPenjualan = dataPenjualanNovel;
+
+  let totalModal = 0,
+    totalKeuntungan = 0,
+    persentaseKeuntungan = 0,
+    theMostSold = 0,
+    filterFromSold = 0,
+    tempProdukTerlaris = 0,
+    tempPenulisTerlaris = 0,
+    resultProdukTerlaris = 0,
+    resultPenulisTerlaris = 0;
+
+  for (let i = 0; i < dataPenjualan.length; i++) {
+    totalModal +=
+      dataPenjualan[i].hargaBeli *
+      (dataPenjualan[i].totalTerjual + dataPenjualan[i].sisaStok);
+    // console.log(totalModal);
+
+    totalKeuntungan +=
+      dataPenjualan[i].hargaJual * dataPenjualan[i].totalTerjual -
+      dataPenjualan[i].hargaBeli *
+        (dataPenjualan[i].totalTerjual + dataPenjualan[i].sisaStok);
+    // console.log(totalKeuntungan);
+
+    persentaseKeuntungan = (totalKeuntungan / totalModal) * 100;
+    // console.log(persentaseKeuntungan);
+
+    theMostSold = Math.max.apply(
+      Math,
+      dataPenjualan.map(function (dataPenjualan) {
+        return dataPenjualan.totalTerjual;
+      })
+    );
+
+    filterFromSold = dataPenjualan.filter((dataPenjualan) => {
+      return dataPenjualan.totalTerjual === theMostSold;
+    });
+
+    tempProdukTerlaris = filterFromSold.map(function (dataPenjualan) {
+      return dataPenjualan.namaProduk;
+    });
+
+    tempPenulisTerlaris = filterFromSold.map(function (dataPenjualan) {
+      return dataPenjualan.penulis;
+    });
+
+    resultProdukTerlaris = tempProdukTerlaris.join();
+    resultPenulisTerlaris = tempPenulisTerlaris.join();
+  }
+  return (hasilAkhir = {
+    totalKeuntungan: idrCurrency.format(totalKeuntungan),
+    totalModal: idrCurrency.format(totalModal),
+    persentaseKeuntungan: persentaseKeuntungan.toFixed(2) + '%',
+    produkBukuTerlaris: resultProdukTerlaris,
+    penulisTerlaris: resultPenulisTerlaris,
+  });
+};
+
+const dataPenjualanNovel = [
+  {
+    idProduct: 'BOOK002421',
+    namaProduk: 'Pulang - Pergi',
+    penulis: 'Tere Liye',
+    hargaBeli: 60000,
+    hargaJual: 86000,
+    totalTerjual: 150,
+    sisaStok: 17,
+  },
+  {
+    idProduct: 'BOOK002351',
+    namaProduk: 'Selamat Tinggal',
+    penulis: 'Tere Liye',
+    hargaBeli: 75000,
+    hargaJual: 103000,
+    totalTerjual: 171,
+    sisaStok: 20,
+  },
+  {
+    idProduct: 'BOOK002941',
+    namaProduk: 'Garis Waktu',
+    penulis: 'Fiersa Besari',
+    hargaBeli: 67000,
+    hargaJual: 99000,
+    totalTerjual: 213,
+    sisaStok: 5,
+  },
+  {
+    idProduct: 'BOOK002941',
+    namaProduk: 'Laskar Pelangi',
+    penulis: 'Andrea Hirata',
+    hargaBeli: 55000,
+    hargaJual: 68000,
+    totalTerjual: 20,
+    sisaStok: 56,
+  },
+];
+
+console.log(getInfoPenjualan(dataPenjualanNovel));
