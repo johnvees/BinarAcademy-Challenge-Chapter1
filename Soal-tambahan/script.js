dcd8e9cef9acaa4dfd35ff82d075d125e44aa42b
johnvees <yohanesvelly23@gmail.com>
2022-02-24 19:04:18 +0700

    done challenge tambahan

---
+++ Soal-tambahan/script.js
@@ -0,0 +1,15 @@
+console.log('Chapter 1 Challenge ++');
+
+const stringToInteger = (angka) => {
+  const angka2 = angka * 1;
+  if (angka2 === angka2) {
+    return angka2;
+  } else {
+    return 'Error : params is not valid';
+  }
+};
+
+console.log(stringToInteger('123'));
+console.log(stringToInteger('456'));
+console.log(stringToInteger('12asd3'));
+console.log(stringToInteger('4sq1'));
