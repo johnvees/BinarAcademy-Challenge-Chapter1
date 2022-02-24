5d279aa8c562b18687a06ace1292a1eaf2298a1f
johnvees <yohanesvelly23@gmail.com>
2022-02-24 17:16:21 +0700

    update return for number 5

--- Soal-5/script.js
+++ Soal-5/script.js
@@ -1,66 +1,66 @@
-console.log("Chapter 1 Challenge Number 5");
+console.log('Chapter 1 Challenge Number 5');
 
 const getSplitName = (personName) => {
-  if (typeof personName === "string") {
-    const panjangNama = personName.split(" ");
+  if (typeof personName === 'string') {
+    const panjangNama = personName.split(' ');
     // return panjangNama;
     if (panjangNama.length === 4) {
-      return "Error: This function is only for 3 words name";
+      return 'Error: This function is only for 3 words name';
     } else if (panjangNama.length === 3) {
       for (let i = 0; i <= panjangNama.length; i++) {
-        const result = panjangNama.slice(0, i + 1).join(" ");
-        const result1 = panjangNama.slice(i + 1, i + 2).join(" ");
-        const result2 = panjangNama.slice(i + 2, i + 3).join(" ");
-        let pemisahNama = {
+        const result = panjangNama.slice(0, i + 1).join(' ');
+        const result1 = panjangNama.slice(i + 1, i + 2).join(' ');
+        const result2 = panjangNama.slice(i + 2, i + 3).join(' ');
+
+        return (pemisahNama = {
           firstName: result,
           middleName: result1,
           lastName: result2,
-        };
-        return pemisahNama;
+        });
       }
     } else if (panjangNama.length === 2) {
       for (let i = 0; i <= panjangNama.length; i++) {
-        const result = panjangNama.slice(0, i + 1).join(" ");
-        const result2 = panjangNama.slice(i + 1, i + 3).join(" ");
-        let pemisahNama = {
+        const result = panjangNama.slice(0, i + 1).join(' ');
+        const result2 = panjangNama.slice(i + 1, i + 3).join(' ');
+
+        return (pemisahNama = {
           firstName: result,
           middleName: null,
           lastName: result2,
-        };
-        return pemisahNama;
+        });
       }
     } else if (panjangNama.length === 1) {
       for (let i = 0; i <= panjangNama.length; i++) {
-        const result = panjangNama.slice(0, i + 1).join(" ");
-        let pemisahNama = {
+        const result = panjangNama.slice(0, i + 1).join(' ');
+
+        return (pemisahNama = {
           firstName: result,
           middleName: null,
           lastName: null,
-        };
-        return pemisahNama;
+        });
       }
     }
   } else {
-    return "ERROR : 0 bukanlah suatu string";
+    return 'ERROR : 0 bukanlah suatu string';
   }
 };
 
 // EXPECTED RESULT
 // Ketika function tersebut dipanggil
 
-console.log(getSplitName("Aldi Daniela Pranata"));
+console.log(getSplitName('Aldi Daniela Pranata'));
 // EXPECTED OUTPUT
 // {firstName : 'Aldi', middleName : 'Daniela', lastName : 'Pranata'}
 
-console.log(getSplitName("Dwi Kuncoro"));
+console.log(getSplitName('Dwi Kuncoro'));
 // EXPECTED OUTPUT
 // {firstName : 'Dwi', middleName : null, lastName : 'Kuncoro'}
 
-console.log(getSplitName("Aurora"));
+console.log(getSplitName('Aurora'));
 // EXPECTED OUTPUT
 // {firstName : 'Aurora', middleName : null, lastName : null}
 
-console.log(getSplitName("Aurora Areliya Sukma Darma"));
+console.log(getSplitName('Aurora Areliya Sukma Darma'));
 // EXPECTED OUTPUT
 // Error : This function is only for 3 word name
 
